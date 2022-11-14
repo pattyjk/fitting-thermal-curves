858d17ad4239a80fe96746debf580f5ff8014fb7
Patrick Kearns <kearnspj23@gmail.com>
2022-11-14 15:10:10 -0500

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,4 +1,4 @@
-install.packages("nls.multstart", lib="/hpcstor6/scratch01/p/patrick.kearns/r_packages")
+install.packages("nls.multstart", lib="/hpcstor6/scratch01/p/patrick.kearns/r_packages", ,repos = "http://cran.us.r-project.org")
 #install.packages("broom")
 #install.packages("tidyverse")
 #install.packages("readr")
