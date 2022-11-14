6fc9a9bc8ea5f283a7afb16275c311d087c5dc88
Patrick Kearns <kearnspj23@gmail.com>
2022-11-14 15:05:25 -0500

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,10 +1,10 @@
-install.packages("nls.multstart")
-install.packages("broom")
-install.packages("tidyverse")
-install.packages("readr")
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("devtools")
+install.packages("nls.multstart", lib="/hpcstor6/scratch01/p/patrick.kearns/r_packages")
+#install.packages("broom")
+#install.packages("tidyverse")
+#install.packages("readr")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("devtools")
 
 #install rTPC
 library(devtools)
