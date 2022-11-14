ce1e36d9b12e31f091e1b0fbc434676db5f72435
Patrick Kearns <kearnspj23@gmail.com>
2022-11-14 14:04:04 -0500

    Create install_packages.R

---
+++ install_packages.R
@@ -0,0 +1,11 @@
+install.packages("nls.multstart")
+install.packages("broom")
+install.packages("tidyverse")
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("devtools")
+
+#install rTPC
+library(devtools)
+install_github("padpadpadpad/rTPC")
